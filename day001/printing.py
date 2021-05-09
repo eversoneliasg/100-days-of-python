27ef9eb9b9c4e59391bf71307fb7913b55c01097
Everson Elias Goncalves de Oliveira <eversoneliasg@gmail.com>
2021-05-09 11:21:20 -0300

    Print, comment, string manipulation, input and variables declaration

---
+++ day001/printing.py
@@ -0,0 +1,13 @@
+print("Hello, World!")
+# Python does not differentiate between single and double quotes when indicating strings.
+print('Hello, single quoted World!')
+# Creating new lines without duplicating commands with backslash and n
+print("Bye, old World!\nHello, new World!")
+# We can also concatenate strings
+print("Hello, " + "Everson!")
+# Entering data with input("A prompt for the user")
+# The program will step inside the print function, to what has to be printed
+# It sees the input function, executes it. The input is replaced by the typed text
+print("Hello, " + input("What is your name? "))
+# Calculating user's name length.
+print(len(input("What is your name? ")))
---
+++ day001/variables.py
@@ -0,0 +1,14 @@
+# The variable will store the data
+name = input("What is your name?")
+print(name)
+# We can change the value atributed to a variable
+name = "Angela"
+print(name)
+# We can use variables to simplify commands, like the one we saw earlier
+length = len(name)
+print(length)
+# Naming variables
+  # make your code readable with meaningful names
+  # don't use space, use underscore, like user_name
+  # can't begin with numbers, for example 2length
+  # don't use privileged words, like functions names
